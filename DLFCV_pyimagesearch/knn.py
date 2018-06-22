9cb75405107e2f53376dc1e7ddc031ef74f44a4a
zhonghy <530634028@qq.com>
2018-06-22 15:31:04 +0800

    add knn for dogs cats pandas

---
+++ DLFCV_pyimagesearch/knn.py
@@ -0,0 +1,9 @@
+#
+# description: implement for knn,need other library
+# a: zhonghy in DLFCV
+# date: 2018-6-22
+#
+
+#import the necessary packages
+from sklearn.neighbors import KNeighborsClassifier
+
