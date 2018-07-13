fa6da2aa05c4a9a9ec4fd8270b3f223819a5b4f5
zhonghy <530634028@qq.com>
2018-07-13 15:20:19 +0800

    Keras for cifar10 dataset

--- DLFCV_pyimagesearch/pyimagesearch.nn/keras_cifar10.py
+++ DLFCV_pyimagesearch/pyimagesearch.nn/keras_cifar10.py
@@ -14,4 +14,50 @@ from keras,layers.core import Dense
 from keras.optimizers import SGD
 from keras.datasets import cifar10
 import tensorflow as tf
-import matplotlib.
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+
+# construct the argument parse and parses the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", required=True,
+                help="path to the output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+# load the training and testing data, scale it int the range [0, 1]
+# then reshape the design matrix
+print("[INFO] loading CIFAR-10 data...")
+((trainX, trainY), (testX, testY)) = cifar10.load_data()
+tainX = trainX.astype("float") / 255.0
+testX = testX.astype("float") / 255.0
+trainX = trainX.reshape((trainX.shape[0], 3072))
+testX = testX.reshape((trainX.shape[0], 3072))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
