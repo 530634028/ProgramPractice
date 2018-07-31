128c28a20bb540852dcb940d74e1ed6082dd264d
zhonghy <530634028@qq.com>
2018-07-31 17:55:45 +0800

    update DLFCV

--- DLFCV/imagenet_pretrained.py
+++ DLFCV/imagenet_pretrained.py
@@ -1,6 +1,7 @@
 #
 # Classify images in ImageNet dataset with pre-trained CNNs
-# using keras library 
+# using keras library
+# need tot download the net from github of keras : attention
 # a: zhonghy
 # date: 2018-7-30
 #
@@ -20,7 +21,7 @@ import argparse
 import cv2
 
 # construct the argument parse and parse the argument
-ap = arparse.ArgumentParser()
+ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="path to the input image")
 ap.add_argument("-model", "--model", type=str, default="vgg16",
@@ -29,65 +30,68 @@ args = vars(ap.parse_args())
 
 # define a dictionary that maps model names to their classes
 # inside Keras
+MODELS = {
+    "vgg16": VGG16,
+    "vgg19": VGG19,
+    "inception": InceptionV3,
+    "xception": Xception, # Tensorflow ONLY
+    "resnet": ResNet50
+    }
+
+# ensure a valid model name was supplied via command line argument
+if args["model"] not in MODELS.keys():
+    raise AssertionError("The --model command line argument should"
+                         "be a key in the 'MODELS' dictionary")
+
+# initialize the input image shape (224x224 pixels) along with
+# the pre-processing function (this might need to be changed
+# based on which model we used to classify our image)
+inputShape = (224, 224)
+preprocess = imagenet_utils.preprocess_input
+
+# if we are using the InceptionV3 or Xception networks, then we
+# need to set the input shape tp (299x299) [rather tha (224x224)]
+# and use a different image processing function
+if args["model"] in ("inception", "xception"):
+    inputShape = (299, 299)
+    preprocess = preprocess_input
+
+# load our the network weights from disk
+print("[INFO] loading {}...".format(args["model"]))
+Network = MODELS[args["model"]]
+model = Network(weights="imagenet")
+
+# load the input image using the keras helper utility with
+# resized to 'inputShape'
+print("[INFO] loading and pre-processing image...")
+image = load_img(args["image"], target_size=inputShape)
+image = img_to_array(image)
+
+# we need to expand the dimension by making the shape (1,
+# inputShape[0], inputShape[1], 3)
+image = np.expand_dims(image, axis=0)
+
+# pre-process the image using the appropriate function based on the
+# model that has been loaded (i.e., mean subtraction, scaling, etc.)
+image = preprocess(image)
+
+# classify the image
+print("[INFO] classifying image with '{}'...".format(args["model"]))
+preds = model.predict(image)
+P = imagenet_utils.decode_predictions(preds)
+
+# loop over the predictions and display the rank-5 predictions +
+# probabilities to our terminal
+for (i, (imagenetID, label, prob)) in enumerate(P[0]):
+    print("{}. {}: {:.2f}%".format(i + 1, label, prob * 100))
+
+# load the image via OpenCV, draw the top prediction on the image
+# and display the image to terminal
+orig = cv2.imread(args["image"])
+(imagenetID, label, prob) = P[0][0]
+cv2.putText(orig, "Label: {}".format(label), (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+cv2.imshow("Classification", orig)
+cv2.waitKey(0)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
