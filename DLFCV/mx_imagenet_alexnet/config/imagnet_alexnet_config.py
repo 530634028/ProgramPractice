06679a38f5b5e2b6e416403955a4ceb21a473d53
zhonghy <530634028@qq.com>
2018-09-30 15:42:47 +0800

    Train CNNs on ImageNet dataset

---
+++ DLFCV/mx_imagenet_alexnet/config/imagnet_alexnet_config.py
@@ -0,0 +1,19 @@
+#
+# Configure file of AlexNet
+# a : zhonghy
+# date: 2018-9-30
+#
+
+# import the necessary packages
+from os import path
+
+# define the base path to where the ImageNet dataset
+# devkit are stored on disk
+BASE_PATH = ""
+
+
+
+
+
+
+
---
+++ DLFCV/pyimagesearch/nn/mxconv/mxalexnet.py
@@ -0,0 +1,35 @@
+#
+# Implement of AlexNet using mxnet library
+# a : zhonghy
+# date: 2018-9-27
+#
+
+# import the necessary packages
+import mxnet as mx
+
+class MxAlexNet:
+    @staticmethod
+    def build(classes):
+        # data input
+        data = mx.sym.Variable("data")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
