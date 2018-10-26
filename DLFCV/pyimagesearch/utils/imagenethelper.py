4ffd2c78d84a9007a43e948819c2225b9efa93ef
zhonghy <530634028@qq.com>
2018-10-26 09:15:31 +0800

    update

--- DLFCV/pyimagesearch/utils/imagenethelper.py
+++ DLFCV/pyimagesearch/utils/imagenethelper.py
@@ -30,6 +30,69 @@ class ImageNetHelper:
             # human readable label
             (wordID, label, hrLabel) = row.split(" ")
 
+            # update the label mappings dictionary using the word ID
+            # as the key and the label as the value, subtracting a"
+            # from the label since MATLAB is one-indexed while Python
+            # is zero-indexed
+            labelMappings[wordID] = int(label) - 1
+
+        # return the label mappings dictionary
+        return labelMappings
+
+    def buildBlacklist(self):
+        # load the list of blacklisted image IDs and convert them to
+        # a set
+        rows = open(self.config.VAL_BLACKLIST).read()
+        rows = set(rows.strip().split("\n"))
+
+        # return the blacklisted image IDs
+        return rows
+
+    def buildTrainingSet(self):
+        # load the contents of the training input file that lists
+        # the partial image ID and image number, then initialize
+        # the list of image paths and class labels
+        rows = open(self.config.TRAIN_LIST).read().strip()
+        rows = rows.split("\n")
+        paths = []
+        labels = []
+
+        # loop over the rows in the input training file
+        for row in rows:
+            # break the row into the partial path and image
+            # number (the image number is sequential and is
+            # essentially useless to us )
+            (partialPath, imageNum) = row.strip().split(" ")
+
+            # construct the full path to the training image, then
+            # grab the word ID from the path and use it to determine
+            # the integer classs label
+            path = os.path.sep.join([self.config.IMAGES_PATH,
+                                     "train", "{}.JEPG".format(partialPath)])
+            wordID = partialPath.split("/")[0]
+            label = self.labelMappings[wordID]
+
+            # update the respective paths and label lists
+            paths.append(path)
+            labels.append(label)
+
+        # return a tuple of image paths and associated integer class
+        # labels
+        return (np.array(paths), np.array(labels))
+
+    def buildValidationSet(self):
+        # initialize the list of image paths and class labels
+        paths = []
+        labels = []
+
+
+
+
+
+
+
+
+
 
 
 
